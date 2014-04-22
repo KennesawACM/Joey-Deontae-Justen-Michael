76cb0f9091f9e215f1f77fe85417060c2642adde
mrhhug <mrhhug@gmail.com>
2014-04-22 13:38:15 -0400

    both shifts working

--- Game.java
+++ Game.java
@@ -1,4 +1,4 @@
-package road;
+import java.util.Random;
 
 public class Game {
 
@@ -10,8 +10,7 @@ public class Game {
 	 * - Is the car suppose to stay at the bottom or top of the screen?da
 	 * - 
 	 */
-	
-	
+
 	/**
 	 * @param args
 	 */
@@ -32,31 +31,22 @@ public class Game {
 
 			}
 		}
-
-		grid[9][1] = 8;
-		printArray(grid);
-
-		/*
-		 * shiftDown(grid); printArray(grid);
-		 */
-
-		shiftLeft(grid);
-		shiftDown(grid);
-		
-
-		printArray(grid);
-
-		/*
-		 * printArray(grid); System.out.println(); grid[0][0] = 9;
-		 * printArray(grid); System.out.println();
-		 * 
-		 * shiftDown(grid); System.out.println(); printArray(grid);
-		 * 
-		 * shiftLeft(grid); System.out.println(); //printArray(grid);
-		 */
-
+		for(int i = 0 ; i <200;i++)
+		{
+			randomShift(grid);
+			printArray(grid);
+		}
 	}
-
+	static void randomShift(int grid[][])
+	{
+		Random rand = new Random();
+		int n = rand.nextInt(3);
+		if(n ==1)
+			shiftLeft(grid);
+		if(n ==2)
+			shiftRight(grid);
+		shiftDown(grid);
+	}
 	static void printArray(int grid[][]) {
 		for (int i = 0; i < grid.length; i++) {
 			for (int j = 0; j < grid.length; j++) {
@@ -65,7 +55,6 @@ public class Game {
 			System.out.print("\n");
 		}
 	}
-
 	static void shiftDown(int grid[][]) {
 		for (int row = C - 1; row > 0; row--) {
 			for (int col = 0; col < C; col++) {
@@ -73,46 +62,25 @@ public class Game {
 			}
 		}
 		System.out.print("\n");
-
 	}
-
-	void shiftRight(int grid[][]) {
-		for (int col = 0; col < C; col++) {
-
-			if (grid[0][0] == 1) {
-				break;
-			} else {
-
-				if ((col + 1) == 21) {
-					grid[col][0] = 0;
-				} else {
-					grid[col][0] = grid[col + 1][0];
-				}
-
+	static void shiftRight(int grid[][]) {
+		if (grid[C-2][0] ==1)
+			return;
+		for (int col = C-1; col > 0; col--) {
+			if ((col + 1) == C)
+				grid[col][0] = 0;
+			else
+			{
+				grid[col][0] = grid[col - 1][0];
 			}
-
 		}
 	}
-
 	static void shiftLeft(int grid[][]) {
-		// if grid[0][0] is road, don't shift
-		for (int col = 0; col < C; col++) {
-
-			if (grid[0][0] == 1) {
-				break;
-			} else {
-
-				if ((col + 1) == 21) {
-					grid[col][0] = 0;
-				} else {
-					grid[col][0] = grid[col + 1][0];
-				}
-
-			}
-
+		if (grid [1][0] ==1)
+			return;	
+		for (int col = 0; col < C-1; col++) {
+			grid[col][0] = grid[col + 1][0];
 		}
-
 		System.out.println();
-
 	}
 }
